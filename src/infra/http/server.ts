4a51565e0fb8598665cd471ad4cdec807b0ff6b3
Matheus Nunes <matheusnunesinfo@gmail.com>
2025-03-15 16:09:33 -0300

    Docs with swagger ui

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,13 +1,20 @@
-import { env } from '@/env'
-import { fastifyCors } from "@fastify/cors";
-import { fastify } from "fastify";
-import { hasZodFastifySchemaValidationErrors, serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
-import { uploadImageRoute } from './routes/upload-image';
+import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
-const server = fastify();
+const server = fastify()
 
-server.setValidatorCompiler(validatorCompiler);
-server.setSerializerCompiler(serializerCompiler);
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
 
 server.setErrorHandler((error, request, reply) => {
   if (hasZodFastifySchemaValidationErrors(error)) {
@@ -16,20 +23,35 @@ server.setErrorHandler((error, request, reply) => {
       issues: error.validation,
     })
   }
-  
+
   // Send the error to observability tools
-  
+
   console.error(error)
-  
+
   return reply.status(500).send({
     message: 'Internal server error',
   })
 })
 
-server.register(fastifyCors, { origin: '*' });
+server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('Server is running on port 3333');
-});
+  console.log('Server is running on port 3333')
+})
