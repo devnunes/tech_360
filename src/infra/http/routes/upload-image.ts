93e5e6d4211057063d69256b898403ace391f496
Matheus Nunes <matheusnunesinfo@gmail.com>
2025-03-16 17:22:23 -0300

    Unique and sortable ids - UUIDV7 setup

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,3 +1,5 @@
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
@@ -20,6 +22,12 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       },
     },
     async (request, reply) => {
+      await db.insert(schema.uploads).values({
+        name: 'teste.jpg',
+        remoteKey: '123',
+        remoteUrl: 'http://sadfasdgf.com',
+      })
+
       return reply.status(201).send({ uploadID: '123' })
     }
   )
