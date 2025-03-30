8da9cbe09c2b51e2420cc46d5f57e4d871938a11
Matheus Nunes <matheusnunesinfo@gmail.com>
2025-03-29 21:43:20 -0300

    Widget collapsible behaviour

--- web/src/components/upload-widget.tsx
+++ web/src/components/upload-widget.tsx
@@ -1,19 +1,30 @@
 import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
+import * as Collapsible from "@radix-ui/react-collapsible";
+import { useState } from "react";
+import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 
 export function UploadWidget() {
+  const [isWidgetOpen, setWidgetOpen] = useState(false);
   return (
-    <div className="bg-zinc-900 w-full max-w-[360px] overflow-hidden rounded-xl shadow-shape">
-      <UploadWidgetHeader />
-      
-      <div className="flex flex-col gap-4 py-3">
-        <UploadWidgetDropzone />
-        
-        <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
-        
-        <UploadWidgetUploadList />
+    <Collapsible.Root onOpenChange={setWidgetOpen}>
+      <div className="bg-zinc-900 w-[360px] overflow-hidden rounded-xl shadow-shape">
+
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
+
+        <Collapsible.Content>
+          <UploadWidgetHeader />
+
+          <div className="flex flex-col gap-4 py-3">
+            <UploadWidgetDropzone />
+
+            <div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
+
+            <UploadWidgetUploadList />
+          </div>
+        </Collapsible.Content>
       </div>
-    </div>
+    </Collapsible.Root>
   );
 }
