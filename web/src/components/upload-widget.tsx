3b948e23aa61b9aec8f70bc68a07768aa98371e7
Matheus Nunes <matheusnunesinfo@gmail.com>
2025-04-05 13:44:06 -0300

    Core animations to upload widget

--- web/src/components/upload-widget.tsx
+++ web/src/components/upload-widget.tsx
@@ -2,14 +2,33 @@ import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
 import * as Collapsible from "@radix-ui/react-collapsible";
-import { useState } from "react";
+import { motion, useCycle } from "motion/react"
 import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 
 export function UploadWidget() {
-  const [isWidgetOpen, setWidgetOpen] = useState(false);
+  const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
   return (
-    <Collapsible.Root onOpenChange={setWidgetOpen}>
-      <div className="bg-zinc-900 w-[360px] overflow-hidden rounded-xl shadow-shape">
+    <Collapsible.Root onOpenChange={() => toggleWidgetOpen()}>
+      <motion.div
+        className="bg-zinc-900 w-[360] max rounded-xl shadow-shape"
+        animate={isWidgetOpen ? 'open' : 'closed'}
+        variants={{
+          closed: {
+            width: 'max-content',
+            height: 44,
+            transition: {
+              type: 'inertia',
+            }
+          },
+          open: {
+            width: 360,
+            height: 'auto',
+            transition: {
+              duration: 0.1,
+            }
+          },
+        }}
+      >
 
         {!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
@@ -24,7 +43,7 @@ export function UploadWidget() {
             <UploadWidgetUploadList />
           </div>
         </Collapsible.Content>
-      </div>
+      </motion.div>
     </Collapsible.Root>
   );
 }
