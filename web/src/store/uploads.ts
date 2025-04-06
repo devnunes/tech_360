b04fd253cc3ccfb71bb0e8ecc2cdceadbd8d5084
Matheus Nunes <matheusnunesinfo@gmail.com>
2025-04-06 13:03:44 -0300

    Feature cancel upload

--- web/src/store/uploads.ts
+++ web/src/store/uploads.ts
@@ -1,3 +1,4 @@
+import { CanceledError } from 'axios'
 import { enableMapSet } from 'immer'
 import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
@@ -8,6 +9,8 @@ export interface Upload {
   file: File
   abortController: AbortController
   status: 'progress' | 'success' | 'error' | 'canceled'
+  originalSizeInBytes: number
+  uploadSizeInBytes: number
 }
 
 type UploadState = {
@@ -29,7 +32,17 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
 
       try {
         await uploadFileToStorage(
-          { file: upload.file },
+          {
+            file: upload.file,
+            onProgress(sizeInBytes) {
+              set(state => {
+                state.uploads.set(uploadId, {
+                  ...upload,
+                  uploadSizeInBytes: sizeInBytes,
+                })
+              })
+            },
+          },
           { signal: upload.abortController.signal }
         )
 
@@ -39,11 +52,11 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
             status: 'success',
           })
         })
-      } catch {
+      } catch (error) {
         set(state => {
           state.uploads.set(uploadId, {
             ...upload,
-            status: 'error',
+            status: error instanceof CanceledError ? 'canceled' : 'error',
           })
         })
       }
@@ -75,6 +88,8 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
           file,
           abortController,
           status: 'progress',
+          originalSizeInBytes: file.size,
+          uploadSizeInBytes: 0,
         }
         set(state => {
           state.uploads.set(uploadId, upload)
